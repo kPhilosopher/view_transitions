4f03b0c2d360690556f93fd62f30d6af8735fe6a
Jinwoo Baek <jbaek@thoughtworks.com>
2014-04-03 00:37:48 -0700

    Loop through a transition of colors and labels

--- Transitions/TRViewController.m
+++ Transitions/TRViewController.m
@@ -8,7 +8,20 @@
 
 #import "TRViewController.h"
 
+#define NUMBER_OF_TRANSITIONS 3
+#define TOTAL_ANIMATION_DURATION 1.2
+#define ANIMATION_START_X_COORDINATE 50
+
 @interface TRViewController ()
+
+{
+    @private
+    NSArray *_labels;
+    NSArray *_colours;
+    
+    NSInteger colourIndex;
+    NSInteger labelIndex;
+}
 
 @end
 
@@ -17,13 +30,69 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    CGRect firstLabelRectangle = CGRectMake(0, 0, 40, 40);
+    UILabel *firstLabel = [[UILabel alloc] initWithFrame:firstLabelRectangle];
+    firstLabel.center = self.view.center;
+    firstLabel.text = @"Hello";
+    [self.view addSubview:firstLabel];
+    
+    CGRect subsequentLabelRectangle = firstLabel.frame;
+    subsequentLabelRectangle.origin.x = CGRectGetMaxX(self.view.frame) + ANIMATION_START_X_COORDINATE;
+    
+    UILabel *secondLabel = [[UILabel alloc] initWithFrame:subsequentLabelRectangle];
+    secondLabel.text = @"Hi";
+    [self.view addSubview:secondLabel];
+    
+    UILabel *thirdLabel = [[UILabel alloc] initWithFrame:subsequentLabelRectangle];
+    thirdLabel.text = @"Hey";
+    [self.view addSubview:thirdLabel];
+
+    
+    _labels = [NSArray arrayWithObjects:firstLabel, secondLabel, thirdLabel, nil];
+    
+    UIColor *firstColor = [UIColor greenColor];
+    UIColor *secondColor = [UIColor blueColor];
+    UIColor *thirdColor = [UIColor magentaColor];
+    self.view.backgroundColor = thirdColor;
+    
+    _colours = [NSArray arrayWithObjects:firstColor, secondColor, thirdColor, nil];
 }
 
-- (void)didReceiveMemoryWarning
+- (void)viewDidAppear:(BOOL)animated;
 {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+    [super viewDidAppear:animated];
+    colourIndex = -1;
+    labelIndex = -1;
+    [NSTimer scheduledTimerWithTimeInterval:3.5
+                                     target:self
+                                   selector:@selector(startTransitioning)
+                                   userInfo:nil
+                                    repeats:YES];
 }
 
+- (void)startTransitioning;
+{
+    colourIndex++;
+    labelIndex++;
+    [UIView animateWithDuration:TOTAL_ANIMATION_DURATION animations:^{
+        self.view.backgroundColor = [_colours objectAtIndex:colourIndex % NUMBER_OF_TRANSITIONS];
+    }];
+    [UIView animateWithDuration:(TOTAL_ANIMATION_DURATION/2) animations:^{
+        UILabel *currentlyDisplayingLabel = (UILabel*)[_labels objectAtIndex:labelIndex % NUMBER_OF_TRANSITIONS];
+        CGRect oldRectangle = currentlyDisplayingLabel.frame;
+        oldRectangle.origin.x = -ANIMATION_START_X_COORDINATE;
+        currentlyDisplayingLabel.frame = oldRectangle;
+
+    } completion:^(BOOL finished){
+        [UIView animateWithDuration:(TOTAL_ANIMATION_DURATION/2) animations:^{
+            UILabel *nextLabel = (UILabel*)[_labels objectAtIndex:(labelIndex + 1) % NUMBER_OF_TRANSITIONS];
+            nextLabel.center = self.view.center;
+        } completion:^(BOOL finished){
+            UILabel *justDisappearedLabel = (UILabel*)[_labels objectAtIndex:labelIndex % NUMBER_OF_TRANSITIONS];
+            CGRect oldRectangle = justDisappearedLabel.frame;
+            oldRectangle.origin.x = CGRectGetMaxX(self.view.frame) + ANIMATION_START_X_COORDINATE;
+            justDisappearedLabel.frame = oldRectangle;
+        }];
+    }];
+}
 @end
